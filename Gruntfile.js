69d87ff9348d0fd0ff16665a756d38f45f8aceff
Yves Delley <hack@delley.net>
2015-06-25 14:11:30 +0200

    include plugins into distribution

--- Gruntfile.js
+++ Gruntfile.js
@@ -137,7 +137,8 @@ module.exports = function(grunt) {
 				files: [
 					{src: ['js/reveal.min.js'], dest: 'dist/'},
 					{src: ['css/reveal.min.css'], dest:'dist/'},
-					{expand: true, src: ['css/theme/*.css'], dest: 'dist/', filter: 'isFile'}
+					{expand: true, src: ['css/theme/*.css'], dest: 'dist/', filter: 'isFile'},
+					{expand: true, src: ['plugin/*'], dest: 'dist/'}
 				]
 			}
 		}
